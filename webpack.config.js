0e8ecbd1f3bbeaeb5e3d827e61c77f250d6f5323
Sergey M Solomkin <lynch513@yandex.ru>
2022-06-10 21:21:46 +0500

    Renamed directory dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,11 @@ const path = require("path")
 module.exports = {
     mode: "none",
     entry: "./src/App.fsproj",
+    output: {
+        "path": path.resolve(__dirname, 'docs')
+    },
     devServer: {
-        contentBase: path.join(__dirname, "./dist")
+        contentBase: path.join(__dirname, "./docs")
     },
     module: {
         rules: [{
